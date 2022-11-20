83c8672ab9464708192c452a4cca191f612cc485
Yuriy Botsev <yurboc@yandex.ru>
2022-11-20 23:36:17 +0300

    Implement MCU_Icon parsing as Object

--- mission.py
+++ mission.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 class Options:
     def __init__(self):
@@ -40,12 +41,32 @@ class Options:
 class MCU_Icon:
     def __init__(self, mcuIconStr=""):
         self.rawData = mcuIconStr
+        self.options = dict()
 
     def loadFromString(self, mcuIconStr):
         self.rawData = mcuIconStr
+        self.options = dict()
 
     def printRawData(self):
         print(self.rawData)
+
+    def parse(self):
+        #print(f"===== ITEM BEGIN =====")
+        self.options = dict()
+        for optionStr in self.rawData.splitlines(keepends=False):
+            if "=" not in optionStr:
+                continue
+            optionStr = optionStr.replace(";", "")
+            optionArr = optionStr.split(" = ")
+            if len(optionArr) != 2:
+                continue
+            optionName = optionArr[0].strip()
+            optionValue = optionArr[1].strip()
+            optionValueObj = json.loads(optionValue)
+            self.options[optionName] = optionValueObj
+            #print(f"{optionName}:{optionValue}")
+        #print(self.options)
+        #print(f"===== ITEM END =====")
 
 class Mission:
     def __init__(self):
@@ -85,6 +106,11 @@ class Mission:
                     foundMCU = False
                     continue
         print(f"Load MCU_Icon done: {itemsCount} items")
+
+        # Parse all MCU_Icon: from String to Object
+        for mcuIcon in self.mcuIcons:
+            mcuIcon.parse()
+        print("Load and parse done!")
 
     def printMissionStat(self):
         print(f"===== BEGIN =====")
