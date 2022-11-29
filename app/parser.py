1abfe316973b8b269f23bc53896aa869f08d0628
Yuriy Botsev <yurboc@yandex.ru>
2022-11-29 04:19:00 +0300

    Implement Telegram Bot

--- app/parser.py
+++ app/parser.py
@@ -4,26 +4,7 @@ import os
 
 from mission import Mission
 
-if __name__ == '__main__':
-    # Prepare all arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--mission-in-file', type=str, dest='mis_in', action='store', help='Path to input mission file')
-    parser.add_argument('-o', '--mission-out-file', type=str, dest='mis_out', action='store', help='Path to output mission file')
-    parser.add_argument('-v', '--mission-img-file', type=str, dest='mis_img', action='store', help='Path to mission visualization')
-    parser.add_argument('-c', '--config-in-file', type=str, dest='conf_in', action='store', help='Path to input JSON config')
-
-    # Show help if no arguments provided
-    if len(sys.argv)==1:
-        parser.print_help(sys.stderr)
-        sys.exit(1)
-
-    # Parse arguments
-    args = parser.parse_args()
-    mission_in_path = args.mis_in
-    mission_out_path = args.mis_out
-    mission_img_path = args.mis_img
-    config_in_path = args.conf_in
-
+def main(mission_in_path, mission_out_path, mission_img_path, config_in_path):
     # Check files
     if not os.path.exists(mission_in_path):
         print(f"Input file is not found: {mission_in_path}")
@@ -52,3 +33,26 @@ if __name__ == '__main__':
     mission.calcVisual()
     mission.plotVisual()
     mission.saveVisual(mission_img_path)
+
+if __name__ == '__main__':
+    # Prepare all arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--mission-in-file', type=str, dest='mis_in', action='store', help='Path to input mission file')
+    parser.add_argument('-o', '--mission-out-file', type=str, dest='mis_out', action='store', help='Path to output mission file')
+    parser.add_argument('-v', '--mission-img-file', type=str, dest='mis_img', action='store', help='Path to mission visualization')
+    parser.add_argument('-c', '--config-in-file', type=str, dest='conf_in', action='store', help='Path to input JSON config')
+
+    # Show help if no arguments provided
+    if len(sys.argv)==1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    # Parse arguments
+    args = parser.parse_args()
+    mission_in_path = args.mis_in
+    mission_out_path = args.mis_out
+    mission_img_path = args.mis_img
+    config_in_path = args.conf_in
+
+    # Run main
+    main(mission_in_path, mission_out_path, mission_img_path, config_in_path)
---
+++ bot/5916024152.py
@@ -0,0 +1,104 @@
+import os
+import subprocess
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import MessageHandler, filters
+
+from priv_data import bot_token
+
+app_python = "python3"
+app_path = os.path.join('..','app','parser.py')
+files_dir = os.path.join("..", "data", "incoming")
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.DEBUG
+)
+
+async def downloader(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    result_file = await context.bot.get_file(update.message.document)
+    orig_file_name = update.message.document.file_name
+    file_name, file_extension = os.path.splitext(orig_file_name)
+    file_name = update.effective_chat.id
+    if file_extension == ".Mission":
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Принял файл миссии!")
+    elif file_extension == ".json":
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Принял файл конфигурации!")
+    else:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Не знаю тип {file_extension}")
+        return
+    new_file_path = os.path.join(files_dir, f"{file_name}{file_extension}")
+    logging.info(f"Saving incoming file to {new_file_path}")
+    await result_file.download_to_drive(new_file_path)
+    logging.info(f"File {new_file_path} saved")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    mission_file_name = os.path.join(files_dir, f"{update.effective_chat.id}.Mission")
+    config_file_name = os.path.join(files_dir, f"{update.effective_chat.id}.json")
+    if os.path.exists(mission_file_name):
+        os.unlink(mission_file_name)
+    if os.path.exists(config_file_name):
+        os.unlink(config_file_name)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Начнём! Закидывай файлы")
+
+async def process(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    mission_in_path = os.path.join(files_dir, f"{update.effective_chat.id}.Mission")
+    config_in_path = os.path.join(files_dir, f"{update.effective_chat.id}.json")
+    mission_out_path = os.path.join(files_dir, f"{update.effective_chat.id}_out.Mission")
+    mission_img_path = os.path.join(files_dir, f"{update.effective_chat.id}.png")
+    parser_log_path = os.path.join(files_dir, f"{update.effective_chat.id}.log")
+    errors = []
+    if not os.path.exists(mission_in_path):
+        errors.append("-- Не загружен файл миссии")
+    if not os.path.exists(config_in_path):
+        errors.append("-- Не загружен файл конфигурации")
+    if errors:
+        errText = '\n'.join(errors)
+        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text=f"Не получилось:\n{errText}")
+        return
+    
+    # Start processing
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text="Начинаем обработку, жди...")
+    cmd = f"{app_python} {app_path} -i {mission_in_path} -o {mission_out_path} -v {mission_img_path} -c {config_in_path}"
+    logging.info(f"Proc start: {cmd}")
+    #res = os.system(cmd)
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    out, err = p.communicate()
+    with open(parser_log_path, 'w') as f:
+        f.write(out.decode())
+    res = 'OK' if not err else err
+    logging.info(f"Proc done: {res}")
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text=f"Готово! Ответ: {res}")
+    if err:
+        return
+    
+    # Send result
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text="Отправляю файлы...")
+    logging.info(f"Begin send files")
+    await update.message.reply_document(
+        document=mission_out_path,
+        caption="Файл линии фронта"
+    )
+    await update.message.reply_document(
+        document=mission_img_path,
+        caption="Файл визуализации"
+    )
+    logging.info(f"Finish send files")
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(bot_token).build()
+    
+    start_handler = CommandHandler('start', start)
+    process_handler = CommandHandler('process', process)
+    file_handler = MessageHandler(filters.Document.ALL, downloader)
+    application.add_handler(start_handler)
+    application.add_handler(process_handler)
+    application.add_handler(file_handler)
+    
+    application.run_polling()
