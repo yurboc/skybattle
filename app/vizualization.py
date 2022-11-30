6dae5c375b9a9b8e9be1e15e2b5fd10f23f931d9
Yuriy Botsev <yurboc@yandex.ru>
2022-12-01 01:38:44 +0300

    Draw front line with arrows

--- app/vizualization.py
+++ app/vizualization.py
@@ -1,3 +1,4 @@
+import math
 from PIL import Image, ImageDraw
 
 class Vizualization:
@@ -23,6 +24,7 @@ class Vizualization:
 
         self.pointR = 3 # point radius (R = D/2)
         self.pointW = 2 # point line width
+        self.arrowSize = 3 # arrow size on the Front Line (in pixels)
         self.colorPointFill = {1: ((200, 150, 150, 0)), 2: ((150, 150, 200, 0))}  # 1: light red, 2: light blue
         self.colorPointLine = {1: ((200, 0, 0, 0)), 2: ((0, 0, 200, 0))}          # 1: red, 2: blue
         self.colorTargetLine = {1: ((255, 200, 200, 0)), 2: ((200, 200, 255, 0))} # 1: light red, 2: light blue
@@ -34,10 +36,10 @@ class Vizualization:
 
         self.img = Image.new('RGB', (self.cnvW, self.cnvH), (230, 230, 230, 0))
         self.draw = ImageDraw.Draw(self.img)
-        self.draw.rectangle([(0, 0), (self.cnvW-1, self.imgY1)], (255, 255, 255, 0), (200, 200, 200, 0))
-        self.draw.rectangle([(0, 0), (self.imgX1, self.cnvH-1)], (255, 255, 255, 0), (200, 200, 200, 0))
-        self.draw.rectangle([(self.cnvW-1, self.cnvH-1), (self.imgX2+1, 0)], (255, 255, 255, 0), (200, 200, 200, 0))
-        self.draw.rectangle([(self.cnvW-1, self.cnvH-1), (0, self.imgY2+1)], (255, 255, 255, 0), (200, 200, 200, 0))
+        self.draw.rectangle([(0, 0), (self.cnvW-1, self.imgY1)], (255, 255, 255, 0))
+        self.draw.rectangle([(0, 0), (self.imgX1, self.cnvH-1)], (255, 255, 255, 0))
+        self.draw.rectangle([(self.cnvW-1, self.cnvH-1), (self.imgX2+1, 0)], (255, 255, 255, 0))
+        self.draw.rectangle([(self.cnvW-1, self.cnvH-1), (0, self.imgY2+1)], (255, 255, 255, 0))
 
     def mapMcuPointToImage(self, mcuPoint):
         y = (mcuPoint.x() - self.minX) * self.kX + self.imgF # map X_of_mcu_point to Y_of_image
@@ -55,6 +57,46 @@ class Vizualization:
         else:
             self.draw.ellipse([(x1,y1),(x2,y2)], self.colorPointFrontFill, self.colorPointFrontLine)
 
+    def arrowedLine(self, ptA, ptB, width=1, color=(0,255,0)):
+        """ Draw line from ptA to ptB with arrowhead at ptB """
+        """ https://stackoverflow.com/questions/63671018/how-can-i-draw-an-arrow-using-pil/63673261#63673261 """
+        # Get drawing context
+        draw = self.draw
+        # Draw the line without arrows
+        draw.line((ptA,ptB), width=width, fill=color)
+
+        # Now work out the arrowhead
+        # = it will be a triangle with one vertex at ptB
+        # - it will start at 95% of the length of the line
+        # - it will extend 3 pixels either side of the line
+        x0, y0 = ptA
+        x1, y1 = ptB
+        # Now we can work out the x,y coordinates of the bottom of the arrowhead triangle
+        xb = 0.95*(x1-x0)+x0
+        yb = 0.95*(y1-y0)+y0
+
+        # Work out the other two vertices of the triangle
+        # Check if line is vertical
+        if x0==x1:
+            vtx0 = (xb-5, yb)
+            vtx1 = (xb+5, yb)
+        # Check if line is horizontal
+        elif y0==y1:
+            vtx0 = (xb, yb+5)
+            vtx1 = (xb, yb-5)
+        else:
+            alpha = math.atan2(y1-y0,x1-x0)-90*math.pi/180
+            a = self.arrowSize*math.cos(alpha)
+            b = self.arrowSize*math.sin(alpha)
+            vtx0 = (xb+a, yb+b)
+            vtx1 = (xb-a, yb-b)
+
+        #draw.point((xb,yb), fill=(255,0,0))    # DEBUG: draw point of base in red - comment out draw.polygon() below if using this line
+        #im.save('DEBUG-base.png')              # DEBUG: save
+
+        # Now draw the arrowhead triangle
+        draw.polygon([vtx0, vtx1, ptB], fill=color)
+
     def placeCaption(self, mcuIcon):
         (x,y) = self.mapMcuPointToImage(mcuIcon)
         y -= 15
@@ -65,8 +107,8 @@ class Vizualization:
         self.draw.text((x,y), str(mcuIcon.options['Index']), fill=textColor, ancor="center")
 
     def placeLine(self, mcuIcon1, mcuIcon2, isFrontLine=False):
-        (x1,y1) = self.mapMcuPointToImage(mcuIcon1)
-        (x2,y2) = self.mapMcuPointToImage(mcuIcon2)
+        ptStart = self.mapMcuPointToImage(mcuIcon1)
+        ptStop = self.mapMcuPointToImage(mcuIcon2)
         if isFrontLine:
             color = self.colorTargetFrontLine
             width = 2
@@ -76,7 +118,10 @@ class Vizualization:
         else:
             color = self.colorTargetLineDiff
             width = 1
-        self.draw.line([(x1,y1),(x2,y2)], color, width)
+        if isFrontLine:
+            self.arrowedLine(ptA=ptStart, ptB=ptStop, width=width, color=color) # with arrow
+        else:
+            self.draw.line([ptStart, ptStop], color, width) # without arrow
 
     def saveToFile(self, filePath):
         self.img.save(filePath, "PNG")
